d168bbf37d91fbb4b8658a52b476ba2b6fd16f12
tomeklarysz <ttomek.larysz@gmail.com>
2024-03-18 00:02:59 +0100

    es6 modules

---
+++ es6modules/anotherFile.js
@@ -0,0 +1,10 @@
+// another JS file
+import { functionOne } from './functionOne';
+
+functionOne(); // this should work as expected!
+
+// By using ES6 modules, you can keep different parts 
+// of your code cleanly separated, which makes writing and 
+// maintaining your code much easier and less error-prone. 
+// Keep in mind that you can definitely export constructors, 
+// classes and factory functions from your modules.
---
+++ es6modules/functionOne.js
@@ -0,0 +1,4 @@
+// a file called functionOne.js
+const functionOne = () => console.log('FUNCTION ONE!');
+
+export { functionOne };
