80024a04e1bed15b86231813b6c06ef6a35317c4
tomeklarysz <ttomek.larysz@gmail.com>
2024-07-13 12:02:35 +0200

    more async and await examples

--- async/await.js
+++ async/await.js
@@ -29,3 +29,58 @@ const server = {
     });
   },
 };
+
+
+async function f() {
+  return 1;
+}
+
+f().then(console.log)
+
+async function r() {
+  await Promise.reject("Failed Promise");
+}
+r().catch(err => console.error(err))
+
+// await works only inside async functions
+// The keyword await makes JavaScript wait 
+// until that promise settles and returns its result.
+
+// It's just a more elegant syntax of getting the promise
+// result than promise.then
+
+async function d() {
+  try {
+    let response = await fetch('http://no-such-url');
+    let user = await response.json();
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+d();
+
+
+// let results = await Promise.all([
+  // fetch(url1),
+  // fetch(url2),
+// ])
+
+function doubleAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 2);
+    }, 2000);
+  });
+}
+
+async function addAsync(x) {
+  const a = await doubleAfter2Seconds(10);
+  const b = await doubleAfter2Seconds(20);
+  const c = await doubleAfter2Seconds(30);
+  return x + a + b + c;
+}
+
+addAsync(10).then(sum => {
+  console.log(sum);
+});
