a45e1cb655d0de90217e91663445dcdc3bb262d5
tomeklarysz <ttomek.larysz@gmail.com>
2024-07-16 21:42:02 +0200

    finished test base methods

--- algo/linked-list-main.mjs
+++ algo/linked-list-main.mjs
@@ -9,5 +9,20 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
-console.log(list)
+
+// tests
+console.log(list.head)  // works
+console.log(list.tail)  // works
+
+list.prepend("skibidi") // works
+console.log('at index 2: ' + list.at(2).value)  // cat - works
+console.log(list.head)
+console.log(list.toString())  // works
+console.log(list.size)   // works
+console.log(list.contains('snake'))   // works
+console.log(list.contains('mbappe'))  // works
+list.pop()
 console.log(list.toString())
+console.log(list.tail)
+
+console.log('parrot index is ' + list.find('parrot'))
