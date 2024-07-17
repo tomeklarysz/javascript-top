c35926c139499d1b9baead72d55cc8c06ed609e6
tomeklarysz <ttomek.larysz@gmail.com>
2024-07-17 12:12:04 +0200

    methods rewrite using arrays inside array

--- algo/HashMap.mjs
+++ algo/HashMap.mjs
@@ -3,9 +3,9 @@ export class HashMap {
   constructor () {
     this.buckets = []
     for (let i=0; i<16; i++) {
-      this.buckets.push('')
+      this.buckets.push([])
     }
-    this.capacity = 16
+    this.capacity = this.buckets.length
     this.load = 0.75
   }
 
@@ -14,71 +14,91 @@ export class HashMap {
        
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
-  
     return hashCode;
   }
 
   set(key, value) {
-    let index = this.hash(key) % this.capacity
+    let index = this.hash(key)
     if (index < 0 || index >= this.capacity) {
       throw new Error("Trying to access index out of bound");
     }
-    this.buckets[index] = value
-    if (this.entries().length >= this.capacity * this.load) {
-      for (let i=this.capacity; i<this.capacity*2; i++) {
-        this.buckets.push('')
+    this.buckets[index].push([key, value])
+    this.capacity = this.buckets.length
+    if (this.length() > this.capacity * this.load) {
+      let i = this.capacity
+      const limit = 2 * this.capacity
+      while (i < limit) {
+        this.buckets.push([])
+        i++
       }
-      this.capacity *= 2
+      this.capacity = this.buckets.length
     }
   }
 
   get(key) {
     let index = this.hash(key)
-    return index <= this.buckets.length ? this.buckets[index] : null
+    for (let pair of this.buckets[index]) {
+      if (pair[0] === key) {
+        return pair[1]
+      }
+    }
+    return null
   }
 
   has(key) {
-    let keys = Array.from(this.buckets.keys())
-    keys.forEach(val => {
-      if (val == key) return true
-    })
+    let index = this.hash(key)
+    for (let pair of this.buckets[index]) {
+      if (pair[0] === key) return true
+    }
     return false
   }
-  
+
   remove(key) {
     if (this.has(key)) {
-      this.buckets.splice(this.hash(key), 1)
+      let index = this.hash(key)
+      for (let i=0; i<this.buckets[index].length; i++) {
+        if (this.buckets[index][i][0] === key) {
+          this.buckets[index].splice(i, 1)
+        }
+      }
+      this.capacity = this.buckets.length
     }
     return this.has(key)
   }
-  
+
   length() {
-    return Array.from(this.buckets.keys()).length
+    let result = 0
+    for (let pairs of this.buckets) {
+      pairs.forEach(() => result++)
+    }
+    return result
   }
 
   clear() {
-    for (let i=0; i<this.buckets.length; i++) {
-      this.buckets[i] = undefined
+    for (let pairs of this.buckets) {
+      while (pairs.length > 0) {
+        pairs.pop()
+      }
     }
   }
 
   keys() {
-    let indexes = []
-    for (let i=0; i<this.buckets.length; i++) {
-      if (this.buckets[i] !== '') {
-        indexes.push(i)
+    let result = []
+    for (let pairs of this.buckets) {
+      for (let pair of pairs) {
+        result.push(pair[0])
       }
     }
-    return indexes
+    return result
   }
-  
+
   values() {
     let result = []
-    for (let i=0; i<this.buckets.length; i++) {
-      if (this.buckets[i] !== '') {
-        result.push(this.buckets[i])
+    for (let pairs of this.buckets) {
+      for (let pair of pairs) {
+        result.push(pair[1])
       }
     }
     return result
@@ -86,9 +106,9 @@ export class HashMap {
 
   entries() {
     let result = []
-    for (let i=0; i<this.buckets.length; i++) {
-      if (this.buckets[i] !== '') {
-        result.push([i, this.buckets[i]])
+    for (let pairs of this.buckets) {
+      for (let pair of pairs) {
+        result.push(pair)
       }
     }
     return result
