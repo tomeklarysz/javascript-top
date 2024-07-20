6de6983afc9c94ea9621ff614a04dd9e60ad100a
tomeklarysz <ttomek.larysz@gmail.com>
2024-07-21 00:25:32 +0200

    insert method

--- algo/Tree.mjs
+++ algo/Tree.mjs
@@ -14,9 +14,33 @@ export class Tree {
     let mid = Math.floor((start + end) / 2)
     let node = new Node(array[mid])
 
-    node.left = this.buildTreeRecursive(array, 0, mid - 1)
+    node.left = this.buildTreeRecursive(array, start, mid - 1)
     node.right = this.buildTreeRecursive(array, mid + 1, end)
     return node
+  }
+
+  insert(value) {
+    let current = this.root
+    let finished = false
+    while (!finished) {
+      if (value === current.value) {
+        console.log('this value is already in bst')
+        return
+      }
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = new Node(value)
+          return
+        }
+        current = current.left
+      } else {
+        if (current.right === null) {
+          current.right = new Node(value)
+          return
+        }
+        current = current.right
+      }
+    }
   }
 }
 
@@ -35,7 +59,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
@@ -44,6 +68,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 let tree = new Tree(array)
-// tree.buildTree(array)
-// prettyPrint(tree.root)
-console.log(tree.root)
+prettyPrint(tree.root)
+console.log();
+tree.insert(10)
+prettyPrint(tree.root)
