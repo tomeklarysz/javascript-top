c2cf8fc1ade420694ed1f5c9c8cd0e91dee77260
tomeklarysz <ttomek.larysz@gmail.com>
2024-08-18 18:42:59 +0200

    loop with var i

--- hoisting/scope.js
+++ hoisting/scope.js
@@ -11,3 +11,10 @@ function check()
 
 let s = check()
 console.log(s())
+
+
+const b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i < 10; i++) {
+  setTimeout(() => console.log(b[i]), 500)
+}
+// logs undefined 10 times
